a9e4fd03181d88afc2a09172d480e29b5c751894
s-a-m-i-o <s.a.m.i.o@hotmail.com>
2024-06-27 23:22:03 -0300

    site mudado completamente

---
+++ src/js/carrossel.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carrossel img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 4000);
+
+showSlide(currentSlide);
